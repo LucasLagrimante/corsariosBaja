bdb19399cd0095436f2cab7a89332896ced563ca
Aluno <Aluno@10.2.3.79>
2016-10-27 11:54:40 -0200

    sexo anal 31

--- src/java/dao/CompeticaoDAO.java
+++ src/java/dao/CompeticaoDAO.java
@@ -14,34 +14,8 @@ import java.util.List;
 import model.Competicao;
 
 public class CompeticaoDAO {
-
-    public static List<Competicao> obterCompeticoes() throws ClassNotFoundException, SQLException {
-        Connection conexao = null;
-        Statement comando = null;
-        List<Competicao> competicoes = new ArrayList<Competicao>();
-        try {
-            conexao = BD.getConexao();
-            comando = conexao.createStatement();
-            ResultSet rs = comando.executeQuery("select * from competicao");
-            while (rs.next()) {
-                Competicao competicao = new Competicao(
-                        rs.getInt("idCompeticao"),
-                        rs.getString("nome"),
-                        rs.getString("data"),
-                        rs.getString("local"),
-                        rs.getString("hora")
-                );
-                competicoes.add(competicao);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            fecharConexao(conexao, comando);
-        }
-        return competicoes;
-    }
-
-    public static void fecharConexao(Connection conexao, Statement comando) {
+    
+ public static void fecharConexao(Connection conexao, Statement comando) {
         try {
             if (comando != null) {
                 comando.close();
@@ -52,6 +26,75 @@ public class CompeticaoDAO {
 
         } catch (SQLException e) {
         }
+    }
+
+    public static void gravar(Competicao competicao) throws SQLException, ClassNotFoundException {
+        Connection conexao = null;
+        try {
+            conexao = BD.getConexao();
+            String sql = "INSERT INTO competicao (idCompeticao , nome, data, hora, local) VALUES (?, ?, ?, ?,?) ";
+            PreparedStatement comando = conexao.prepareStatement(sql);
+            comando.setInt(1, competicao.getIdCompeticao());
+            comando.setString(2, competicao.getNome());
+            comando.setString(3, competicao.getData());
+            comando.setString(4, competicao.getHora());
+            comando.setString(5, competicao.getLocal());
+            comando.execute();
+            comando.close();
+            conexao.close();
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+
+    public static List<Competicao> obterCompeticoes() throws ClassNotFoundException, SQLException {
+        Connection conexao = null;
+        Statement comando = null;
+        List<Competicao> competicaoes = new ArrayList<Competicao>();
+        try {
+            conexao = BD.getConexao();
+            comando = conexao.createStatement();
+            ResultSet rs = comando.executeQuery("SELECT * FROM competicao");
+            while (rs.next()) {
+                Competicao competicao = new Competicao(
+                   rs.getInt("idCompeticao"),
+                   rs.getString("nome"),
+                   rs.getString("data"),
+                   rs.getString("hora"),
+                   rs.getString("local")
+                );
+                competicaoes.add(competicao);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            fecharConexao(conexao, comando);
+        }
+        return competicaoes;
+    }
+
+    public static Competicao obterCompeticao(int idCompeticao) throws ClassNotFoundException {
+        Connection conexao = null;
+        Statement comando = null;
+        Competicao competicao = null;
+        try {
+            conexao = BD.getConexao();
+            comando = conexao.createStatement();
+            ResultSet rs = comando.executeQuery("SELECT * FROM competicao where idCompeticao = " + idCompeticao);
+            rs.first();
+            competicao = new Competicao(
+                   rs.getInt("idCompeticao"),
+                   rs.getString("nome"),
+                   rs.getString("data"),
+                   rs.getString("hora"),
+                   rs.getString("local")
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            fecharConexao(conexao, comando);
+        }
+        return competicao;
     }
 
 }
